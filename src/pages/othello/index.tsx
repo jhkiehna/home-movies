88e2877d4e6edf9e2190b78b6b48e25a06ad6405
Henry Kiehna <jhkieha@gmail.com>
2023-01-02 19:13:44 -0500

    fixing bug in determining winning player

--- src/pages/othello/index.tsx
+++ src/pages/othello/index.tsx
@@ -11,10 +11,9 @@ import Info from '../../components/othello/Info';
 const Othello: NextPage = () => {
   const [gameState, setGameState] = React.useState<GameState | null>(null);
   const [isGameOver, setIsGameOver] = React.useState(false);
-  const [winningPlayer, setWinningPlayer] = React.useState<'white' | 'black' | null>(null);
-
-  const whiteScore = Object.values(gameState?.board ?? {}).filter((cell) => cell.occupiedBy === 'white').length;
-  const blackScore = Object.values(gameState?.board ?? {}).filter((cell) => cell.occupiedBy === 'black').length;
+  const [winningPlayer, setWinningPlayer] = React.useState<'White' | 'Black' | null>(null);
+  const [blackScore, setBlackScore] = React.useState(0);
+  const [whiteScore, setWhiteScore] = React.useState(0);
 
   React.useEffect(() => {
     // Initialize game board
@@ -25,19 +24,26 @@ const Othello: NextPage = () => {
       return;
     }
 
+    setWhiteScore(Object.values(gameState?.board ?? {}).filter((cell) => cell.occupiedBy === 'white').length);
+    setBlackScore(Object.values(gameState?.board ?? {}).filter((cell) => cell.occupiedBy === 'black').length);
+
+    // Check end game conditions
     const everyCellWhiteOrNull = Object.values(gameState.board).every(
       (cell) => cell.occupiedBy === 'white' || !cell.occupiedBy,
     );
     const everyCellBlackOrNull = Object.values(gameState.board).every(
       (cell) => cell.occupiedBy === 'black' || !cell.occupiedBy,
     );
-    // Check end game conditions
     const boardFilled = Object.values(gameState.board).every((cell) => !!cell.occupiedBy);
     const noMorePossibleMoves = !gameState.possibleMoves.length && (everyCellBlackOrNull || everyCellWhiteOrNull);
 
     if (boardFilled || noMorePossibleMoves) {
       setIsGameOver(true);
-      setWinningPlayer(everyCellWhiteOrNull ? 'white' : 'black');
+
+      if (everyCellWhiteOrNull) return setWinningPlayer('White');
+      if (everyCellBlackOrNull) return setWinningPlayer('Black');
+
+      setWinningPlayer(whiteScore > blackScore ? 'White' : 'Black');
     }
   }, [gameState]);
 
